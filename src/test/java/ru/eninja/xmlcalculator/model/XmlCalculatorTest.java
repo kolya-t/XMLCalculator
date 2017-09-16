da6e4480c91d76b433a5f780f44eb4d5b72bc319
kolya-t <toporkov.kolya96@gmail.com>
2017-09-16 22:28:05 +0300

    Xml tests

--- src/test/java/ru/eninja/xmlcalculator/model/XmlCalculatorTest.java
+++ src/test/java/ru/eninja/xmlcalculator/model/XmlCalculatorTest.java
@@ -1,11 +1,13 @@
-package ru.eninja.xmlcalculator;
+package ru.eninja.xmlcalculator.model;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
-import ru.eninja.xmlcalculator.model.XmlCalculator;
+import ru.eninja.xmlcalculator.XmlCalculator;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -14,15 +16,17 @@ import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
-import static ru.eninja.xmlcalculator.model.XmlCalculator.XSD_FILENAME;
+import static ru.eninja.xmlcalculator.XmlCalculator.XSD_FILENAME;
 
 public class XmlCalculatorTest {
 
     private static final String SRC_DIR = "src/test/resources";
     private static XmlCalculator xmlCalculator;
     private static Validator validator;
+    private static XmlMapper mapper;
 
     @BeforeClass
     public static void initData() throws SAXException {
@@ -30,12 +34,25 @@ public class XmlCalculatorTest {
         validator = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
                 .newSchema(new StreamSource(XmlCalculator.class.getClassLoader().getResourceAsStream(XSD_FILENAME)))
                 .newValidator();
+        mapper = new XmlMapper();
+    }
+
+    @AfterClass
+    public static void deleteResultXmls() {
+        Pattern resultPattern = Pattern.compile(".+Result\\.xml");
+        String[] fileList = new File(SRC_DIR).list();
+        if (fileList != null) {
+            for (String filename : fileList) {
+                if (resultPattern.matcher(filename).matches()) {
+                    new File(SRC_DIR, filename).delete();
+                }
+            }
+        }
     }
 
     @Test
     public void sampleTest() throws IOException, SAXException {
-        calculate("sampleTest.xml", "sampleTestResult.xml");
-        validate("sampleTestResult.xml");
+        assertCalculationsCorrect("sampleTest");
     }
 
     @Test(expected = FileNotFoundException.class)
@@ -55,14 +72,12 @@ public class XmlCalculatorTest {
 
     @Test
     public void oneExpressionTest() throws IOException, SAXException {
-        calculate("oneExpressionTest.xml", "oneExpressionTestResult.xml");
-        validate("oneExpressionTestResult.xml");
+        assertCalculationsCorrect("oneExpressionTest");
     }
 
     @Test
     public void threeExpressionsTest() throws IOException, SAXException {
-        calculate("threeExpressionsTest.xml", "threeExpressionsTestResult.xml");
-        validate("threeExpressionsTestResult.xml");
+        assertCalculationsCorrect("threeExpressionsTest");
     }
 
     @Test
@@ -73,24 +88,31 @@ public class XmlCalculatorTest {
         validate("twoXmlsTest2Result.xml");
     }
 
-    @AfterClass
-    public static void deleteResultXmls() {
-        Pattern resultPattern = Pattern.compile(".+Result\\.xml");
-        String[] fileList = new File(SRC_DIR).list();
-        if (fileList != null) {
-            for (String filename : fileList) {
-                if (resultPattern.matcher(filename).matches()) {
-                    new File(SRC_DIR, filename).delete();
-                }
-            }
-        }
-    }
-
     private void calculate(String srcFilename, String destFilename) throws IOException, SAXException {
         xmlCalculator.calculate(new File(SRC_DIR, srcFilename), new File(SRC_DIR, destFilename));
     }
 
     private void validate(String dstFilename) throws IOException, SAXException {
         validator.validate(new StreamSource(new File(SRC_DIR, dstFilename)));
+    }
+
+    private void assertCalculationsCorrect(String testName) throws IOException, SAXException {
+        String srcFilename = testName + ".xml";
+        String destFilename = testName + "Result.xml";
+        String expectedFilename = testName + "Expected.xml";
+
+        calculate(srcFilename, destFilename);
+        validate(destFilename);
+
+        SimpleCalculator expected = loadCalculatorFromXml(expectedFilename);
+        SimpleCalculator calculated = loadCalculatorFromXml(destFilename);
+        Assert.assertArrayEquals(
+                Arrays.stream(expected.getResults()).mapToDouble(ExpressionResult::getResult).toArray(),
+                Arrays.stream(calculated.getResults()).mapToDouble(ExpressionResult::getResult).toArray(),
+                1e-5);
+    }
+
+    private SimpleCalculator loadCalculatorFromXml(String filename) throws IOException {
+        return mapper.readValue(new File(SRC_DIR, filename), SimpleCalculator.class);
     }
 }
